a68dec5b1d06ea4416bb4bc4763409e3d6e26241
Jacob Brown <Jacob_E_Brown@protonmail.com>
2025-10-10 10:36:40 -0600

    Testing

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -1,9 +1,5 @@
 import React from 'react'
-import TechBadge from './TechBadge'
-import Js from './icons/Js'
-import ReactIcon from './icons/React'
-import Python from './icons/Python'
-import Figma from './icons/Figma'
+import HoneycombGrid from './HoneycombGrid'
 
 // Category icons
 import vector3 from '@/assets/images/TechLogos/vector-3.svg'
@@ -45,47 +41,37 @@ import githubIcon from '@/assets/images/TechLogos/github-1-1.svg'
 import sqlIcon from '@/assets/images/TechLogos/sql.svg'
 
 const technologies = [
-  { name: 'Figma', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="Figma" src={figmaIcon} />, position: 'top-[114px] left-[43px]' },
-  { name: 'My Education', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="Education" src={educationIcon} />, position: 'top-[114px] left-[130px]' },
-  { name: 'AWS', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="AWS" src={awsIcon} />, position: 'top-[114px] left-[219px]' },
-  { name: 'React', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="React" src={reactIcon} />, position: 'top-[202px] left-0' },
-  { name: 'React', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="React" src={reactIcon} />, position: 'top-[201px] left-[87px]' },
-  { name: 'Vite JS', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="Vite" src={vitejsIcon} />, position: 'top-[201px] left-[179px]' },
-  { name: 'Tailwinds CSS', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="Tailwind" src={tailwindIcon} />, position: 'top-[201px] left-[265px]' },
-  { name: 'C#', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="C#" src={csharpIcon} />, position: 'top-[289px] left-11' },
-  { name: 'ASP.NET Core', icon: <img className="relative w-[33.65px] h-[33.65px]" alt=".NET" src={netcoreIcon} />, position: 'top-[289px] left-[131px]' },
-  { name: 'BootStrap', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="Bootstrap" src={bootstrapIcon} />, position: 'top-[289px] left-[217px]' },
-  { name: 'CSS 3', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="CSS" src={cssIcon} />, position: 'top-[375px] left-[3px]' },
-  { name: 'HTML 5', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="HTML" src={htmlIcon} />, position: 'top-[375px] left-[89px]' },
-  { name: 'Java Script', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="JavaScript" src={jsIcon} />, position: 'top-[375px] left-44' },
-  { name: 'Type Script', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="TypeScript" src={tsIcon} />, position: 'top-[375px] left-[262px]' },
-  { name: 'Kotlin', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="Kotlin" src={kotlinIcon} />, position: 'top-[462px] left-[47px]' },
-  { name: 'Jetpack Compose', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="Jetpack" src={jetpackIcon} />, position: 'top-[462px] left-[134px]' },
-  { name: 'Android', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="Android" src={androidIcon} />, position: 'top-[462px] left-[221px]' },
-  { name: 'Linux', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="Linux" src={linuxIcon} />, position: 'top-[549px] left-1' },
-  { name: 'Arch Linux', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="Arch" src={archIcon} />, position: 'top-[549px] left-[91px]' },
-  { name: 'Python', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="Python" src={pythonIcon} />, position: 'top-[549px] left-[178px]' },
-  { name: 'Django', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="Django" src={djangoIcon} />, position: 'top-[549px] left-[265px]' },
-  { name: 'Git and Github', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="GitHub" src={githubIcon} />, position: 'top-[636px] left-[47px]' },
-  { name: 'SQL / Databases', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="SQL" src={sqlIcon} />, position: 'top-[636px] left-[133px]' },
-  { name: 'Linux', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="Linux" src={linuxIcon} />, position: 'top-[636px] left-[219px]' },
+  { name: 'Figma', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="Figma" src={figmaIcon} /> },
+  { name: 'My Education', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="Education" src={educationIcon} /> },
+  { name: 'AWS', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="AWS" src={awsIcon} /> },
+  { name: 'React', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="React" src={reactIcon} /> },
+  { name: 'Vite JS', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="Vite" src={vitejsIcon} /> },
+  { name: 'Tailwinds CSS', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="Tailwind" src={tailwindIcon} /> },
+  { name: 'C#', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="C#" src={csharpIcon} /> },
+  { name: 'ASP.NET Core', icon: <img className="relative w-[33.65px] h-[33.65px]" alt=".NET" src={netcoreIcon} /> },
+  { name: 'BootStrap', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="Bootstrap" src={bootstrapIcon} /> },
+  { name: 'CSS 3', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="CSS" src={cssIcon} /> },
+  { name: 'HTML 5', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="HTML" src={htmlIcon} /> },
+  { name: 'Java Script', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="JavaScript" src={jsIcon} /> },
+  { name: 'Type Script', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="TypeScript" src={tsIcon} /> },
+  { name: 'Kotlin', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="Kotlin" src={kotlinIcon} /> },
+  { name: 'Jetpack Compose', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="Jetpack" src={jetpackIcon} /> },
+  { name: 'Android', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="Android" src={androidIcon} /> },
+  { name: 'Linux', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="Linux" src={linuxIcon} /> },
+  { name: 'Arch Linux', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="Arch" src={archIcon} /> },
+  { name: 'Python', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="Python" src={pythonIcon} /> },
+  { name: 'Django', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="Django" src={djangoIcon} /> },
+  { name: 'Git and Github', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="GitHub" src={githubIcon} /> },
+  { name: 'SQL / Databases', icon: <img className="relative w-[33.65px] h-[33.65px]" alt="SQL" src={sqlIcon} /> },
 ]
 
 export function Technologies(): React.JSX.Element {
   return (
-    <div className="relative w-[351px] h-[723px]">
-      {technologies.map((tech, index) => (
-        <TechBadge
-          key={index}
-          icon={tech.icon}
-          name={tech.name}
-          className={`${tech.position} absolute`}
-        />
-      ))}
-
-      <div className="flex flex-wrap w-[344px] items-center gap-[7px_5px] absolute top-0 left-[calc(50%_-_176px)]">
+    <div className="flex flex-col items-center gap-8 p-6">
+      {/* Category filters */}
+      <div className="flex flex-wrap items-center gap-[7px_5px] justify-center">
         {categories.map((category, index) => (
-          <div key={index} className={`inline-flex items-center justify-center gap-[4.49px] px-[5.62px] py-[4.49px] relative flex-[0_0_auto] ${category.active ? 'bg-blue-medium-1 border-blue-medium-2' : 'bg-cream-neutral border-blue-medium-1'} rounded-[31.64px] overflow-hidden border-[1.31px] border-solid shadow-[0px_1.7px_1.7px_#00000040]`}>
+          <div key={index} className={`category-filter inline-flex items-center justify-center gap-[4.49px] px-[5.62px] py-[4.49px] relative flex-[0_0_auto] ${category.active ? 'bg-blue-medium-1 border-blue-medium-2' : 'bg-cream-neutral border-blue-medium-1'} rounded-[31.64px] overflow-hidden border-[1.31px] border-solid shadow-[0px_1.7px_1.7px_#00000040]`}>
             <div className={`relative flex items-center justify-center w-fit ${category.active ? 'mt-[-1.17px]' : 'mt-[-1.31px]'} font-medium text-text-1 text-[11.2px] tracking-[0.45px] leading-[normal] whitespace-nowrap`}>
               {category.name}
             </div>
@@ -93,6 +79,9 @@ export function Technologies(): React.JSX.Element {
           </div>
         ))}
       </div>
+
+      {/* Honeycomb grid */}
+      <HoneycombGrid technologies={technologies} />
     </div>
   )
 }
