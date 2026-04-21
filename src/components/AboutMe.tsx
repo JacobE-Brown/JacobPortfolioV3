cb347aad6e81270cf489afb9f4324b0d8127acf3
Jacob Brown <Jacob_E_Brown@protonmail.com>
2026-04-21 08:20:01 -0600

    Updated about me.

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -6,10 +6,15 @@ export default function AboutMe() {
           <span className="border-b-4 border-blue-medium-1 pb-2">About Me</span>
         </h2>
         <p className="font-sans text-lg md:text-xl lg:text-2xl text-text-1 leading-relaxed">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ac augue id mi dictum
-          facilisis. Sed vel justo sed nisl malesuada tincidunt vel in lorem. Integer vitae dapibus
-          nisl, at porta augue. Mauris tincidunt, lacus id placerat feugiat, elit justo feugiat odio,
-          at ultricies mi libero vel lorem.
+          I am a DevOps Engineer for Netacent. I recently graduated from CWI with an associates in
+          Full-Stack Web Development, and I like to branch out into extra technologies through online
+          courses, self study, and personal projects. I am naturally strongest in backend code, but I
+          have recently nurtured an interest in good design theory and UI. A couple of my favorite
+          technologies are Amazon Web Services and Figma, which I used for building this portfolio.
+        </p>
+        <p className="font-sans text-lg md:text-xl lg:text-2xl text-text-1 leading-relaxed">
+          My AI philosophy is to give the LLM very refined prompts, always understand what the LLM
+          did and why, asking it for explanations if necessary, and to check and recheck for errors.
         </p>
       </div>
     </section>
